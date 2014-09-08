03faf68672ec4afc4d36f97681553990f8e0cbc8
Tedkirk-ashesi <kirkamoah@gmail.com>
2014-09-08 14:23:09 +0000

    initial project files for step-2

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,10 +2,13 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('PhoneListCtrl' function() {
+	beforeEach(module('phonecatApp'));
+  it('should create "phones" model with 3 phones', inject(function($controller) {
+		var scope = {},
+        ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
-  it("should do something", function() {
-
-  });
+    expect(scope.phones.length).toBe('World');
+  }));
 
 });
